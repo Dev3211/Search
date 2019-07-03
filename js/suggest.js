2e66ec3e027a9426a4edacda030455c41acde9d1
Dev <devsarma321@gmail.com>
2019-07-03 12:50:21 +0530

    Added Keyword suggestion

---
+++ js/suggest.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(document).on('keyup','#search',function(){   
+        let value = $(this).val();
+        $.getJSON('../suggest.php?key=' + value, function (data) {
+            let availableTags = data;
+            $("#search").autocomplete({
+                source: availableTags,
+				appendTo: "#containerr",
+                select: function(event, ui) {
+                $(event.target).val(ui.item.value);
+                $('#form').submit();
+                return false;
+            },
+             });
+        });        
+    });
+});
